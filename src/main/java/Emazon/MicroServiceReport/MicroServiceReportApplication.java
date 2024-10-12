ea89ee812087ec64df054b1e31abfcf6a3e0343a
veronica <105020892+vero2002257257@users.noreply.github.com>
2024-10-11 23:28:20 -0500

    Creación del repositorio

---
+++ src/main/java/Emazon/MicroServiceReport/MicroServiceReportApplication.java
@@ -0,0 +1,13 @@
+package Emazon.MicroServiceReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Emazon/MicroServiceReport/MicroServiceReportApplicationTests.java
@@ -0,0 +1,13 @@
+package Emazon.MicroServiceReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
